4b3a16833cd24ca484f56411ee110dca2fc6f2c1
Arrakis <arrakis@Arrakis.local>
2023-08-28 02:10:01 -0400

    more charts

--- app.py
+++ app.py
@@ -39,9 +39,37 @@ selected_years = st.slider("select a range of draft years to look at",
               max_value=max_year, 
               step=1)
 
-### select df2
+### select dfs
 select_df1 = filter_df(df1, selected_years, selected_pos)
 select_df2 = filter_df(df2, selected_years, selected_pos)
+
+### games by pick no
+games_by_pick = alt.Chart(select_df1).mark_circle(size=75).encode(
+    x=alt.X('pick', title='draft pick number'),
+    y=alt.Y(
+        'g', 
+        title='games played',
+        scale=alt.Scale(domain=(0, select_df1['g'].max()))
+        ),
+    tooltip=['player', 'pos', 'g'],
+    color=alt.Color('draft_year', scale=alt.Scale(scheme='darkblue'))
+)
+
+games_by_pick = games_by_pick.configure_legend(
+    orient='left'
+)
+
+### games by second contract
+games_by_second = alt.Chart(select_df2).mark_circle(size=75).encode(
+    x=alt.X('gtd_norm', title='nornalized contract value'),
+    y=alt.Y(
+        'g', 
+        title='games_played', 
+        scale=alt.Scale(domain=(0, select_df2['g'].max()))
+        ),
+    tooltip=['player', 'pos', 'g'],
+    color=alt.Color('draft_year', scale=alt.Scale(scheme='darkblue'))
+)
 
 ### year one contracts
 year_one_plot = alt.Chart(select_df1).mark_circle(size=75).encode(
@@ -82,7 +110,11 @@ scatterplot = alt.Chart(select_df2).mark_circle(size=75).encode(
 ### show
 col1, col2= st.columns(2)
 
+col1.subheader('games played by pick no.')
+col1.altair_chart(games_by_pick, use_container_width=True, theme='streamlit')
 col1.subheader('rookie contracts by pick no.')
 col1.altair_chart(year_one_plot, use_container_width=True, theme='streamlit')
+col2.subheader('games played by second contract value')
+col2.altair_chart(games_by_second, use_container_width=True, theme='streamlit')
 col2.subheader('second contracts by pick no.')
 col2.altair_chart(year_two_plot, use_container_width=True, theme='streamlit')
