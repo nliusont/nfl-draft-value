6ab8ab0a7c143e4c50a82d7ecf3d994cc261a4b6
Arrakis <arrakis@Arrakis.local>
2023-08-28 15:57:53 -0400

    layout updates and total earnings chart

--- app.py
+++ app.py
@@ -5,11 +5,8 @@ import numpy as np
 
 st.set_page_config(page_title="Analyzing the contract values of NFL draft picks", layout="wide")
 st.title("Analyzing the contract values of NFL draft picks")
-st.subheader('if player contract values reflect player quality, do higher draft picks leader to better players?')
-st.text('Data scraped from the following sources:')
-st.write('[Pro Football Reference](%s)' % https://www.pro-football-reference.com/draft/)
-st.write('[Over The Cap](%s)' % https://overthecap.com/contract-history)
-st.write('[Spotrac](%s)' % https://www.spotrac.com/nfl/cba/)
+st.write('If player contract values reflect player quality, do higher draft picks leader to better players?')
+
 
 ### defs
 
@@ -20,34 +17,47 @@ def filter_df(df, year_selection, pos_selection):
                     & (df['draft_year'] <= year_selection[1]) \
                     & (df['pos'].isin(pos_selection))
     new_df = df[criteria].copy()
+    new_df['draft_year'] = new_df['draft_year'].astype(str)
     return new_df
 
 ### imports
 df1 = pd.read_pickle('data/all_1_contracts.pkl')
 df2 = pd.read_pickle('data/all_2_contracts.pkl')
+df = pd.read_pickle('data/combined_data_2000-2023.pkl')
 
-### selectors
+### selectors sidebar
 min_year = df1['draft_year'].min()
 max_year = df1['draft_year'].max()
 positions = list(np.sort(df1['pos'].unique()))
 pos_list = ['All positions'] + positions
 
+with st.sidebar:
+    selected_pos = st.multiselect("select positions", 
+                                options=pos_list,
+                                default='QB'
+                                )
 
-
-selected_pos = st.multiselect("select positions", 
-                              options=pos_list,
-                              default='QB'
-                              )
-
-selected_years = st.slider("select a range of draft years to look at", 
-              value=[min_year, max_year],
-              min_value=min_year, 
-              max_value=max_year, 
-              step=1)
+    selected_years = st.slider("select a range of draft years to look at", 
+                value=[min_year, max_year],
+                min_value=min_year, 
+                max_value=max_year, 
+                step=1)
 
 ### select dfs
 select_df1 = filter_df(df1, selected_years, selected_pos)
 select_df2 = filter_df(df2, selected_years, selected_pos)
+
+df = df[['player', 'gtd_norm', 'draft_year', 'pos', 'pick','search_key', 'g']]\
+        .groupby('search_key')\
+        .agg({
+            'player':'max',
+            'draft_year':'mean',
+            'gtd_norm': 'sum',
+            'pos':'max',
+            'pick':'mean',
+            'g':'mean'})\
+        .reset_index()  # group by player
+select_df = filter_df(df, selected_years, selected_pos)
 
 ### games by pick no
 games_by_pick = alt.Chart(select_df1).mark_circle(size=75).encode(
@@ -57,24 +67,23 @@ games_by_pick = alt.Chart(select_df1).mark_circle(size=75).encode(
         title='games played',
         scale=alt.Scale(domain=(0, select_df1['g'].max()))
         ),
-    tooltip=['player', 'pos', 'g'],
+    tooltip=['player', 'pick', 'pos', 'g'],
     color=alt.Color('draft_year', scale=alt.Scale(scheme='darkblue'))
 )
 
-games_by_pick = games_by_pick.configure_legend(
-    orient='left'
-)
-
-### games by second contract
-games_by_second = alt.Chart(select_df2).mark_circle(size=75).encode(
-    x=alt.X('gtd_norm', title='nornalized contract value'),
+### total earnings by pick no.
+total_earnings = alt.Chart(select_df).mark_circle(size=75).encode(
+    x=alt.X('pick', title='draft pick number'),
     y=alt.Y(
-        'g', 
-        title='games_played', 
-        scale=alt.Scale(domain=(0, select_df2['g'].max()))
+        'gtd_norm', 
+        title='guaranteed money (normalized)'
         ),
-    tooltip=['player', 'pos', 'g'],
+    tooltip=['player', 'pick', 'pos', 'g'],
     color=alt.Color('draft_year', scale=alt.Scale(scheme='darkblue'))
+)
+
+total_earnings = total_earnings.configure_legend(
+    orient='left'
 )
 
 ### year one contracts
@@ -82,10 +91,10 @@ year_one_plot = alt.Chart(select_df1).mark_circle(size=75).encode(
     x=alt.X('pick', title='draft pick number'),
     y=alt.Y(
         'gtd_norm', 
-        title='normalized contract value',
+        title='guaranteed money (normalized)',
         scale=alt.Scale(domain=(0, select_df2['gtd_norm'].max()))
         ),
-    tooltip=['player', 'pos', alt.Tooltip('gtd_norm', format='.2f')],
+    tooltip=['player', 'pick', 'pos', alt.Tooltip('gtd_norm', format='.2f')],
     color=alt.Color('draft_year', scale=alt.Scale(scheme='darkblue'))
 )
 
@@ -98,10 +107,10 @@ year_two_plot = alt.Chart(select_df2).mark_circle(size=75).encode(
     x=alt.X('pick', title='draft pick number'),
     y=alt.Y(
         'gtd_norm', 
-        title='normalized contract value', 
+        title='guaranteed money (normalized)', 
         scale=alt.Scale(domain=(0, select_df2['gtd_norm'].max()))
         ),
-    tooltip=['player', 'pos', alt.Tooltip('gtd_norm', format='.2f')],
+    tooltip=['player', 'pick', 'pos', alt.Tooltip('gtd_norm', format='.2f')],
     color=alt.Color('draft_year', scale=alt.Scale(scheme='darkblue'))
 )
 
@@ -116,11 +125,22 @@ scatterplot = alt.Chart(select_df2).mark_circle(size=75).encode(
 ### show
 col1, col2= st.columns(2)
 
-col1.subheader('games played by pick no.')
-col1.altair_chart(games_by_pick, use_container_width=True, theme='streamlit')
-col1.subheader('rookie contracts by pick no.')
+col1.markdown("<h3 style='text-align: center;'>rookie contracts</h3>", unsafe_allow_html=True)
 col1.altair_chart(year_one_plot, use_container_width=True, theme='streamlit')
-col2.subheader('games played by second contract value')
-col2.altair_chart(games_by_second, use_container_width=True, theme='streamlit')
-col2.subheader('second contracts by pick no.')
+col1.markdown("<h3 style='text-align: center;'>total guaranteed money</h3>", unsafe_allow_html=True)
+col1.altair_chart(total_earnings, use_container_width=True, theme='streamlit')
+col2.markdown("<h3 style='text-align: center;'>second contracts</h3>", unsafe_allow_html=True)
 col2.altair_chart(year_two_plot, use_container_width=True, theme='streamlit')
+col2.markdown("<h3 style='text-align: center;'>games played</h3>", unsafe_allow_html=True)
+col2.altair_chart(games_by_pick, use_container_width=True, theme='streamlit')
+
+# refs
+pfr = 'https://www.pro-football-reference.com/draft/'
+otc = 'https://overthecap.com/contract-history'
+sptrc = 'https://www.spotrac.com/nfl/cba/'
+st.write('Sources: ')
+st.write('[Pro Football Reference](%s)' % pfr)
+st.write('[Over The Cap](%s)' % otc)
+st.write('[Spotrac](%s)' % sptrc)
+
+year_one_plot.configure_legend()
